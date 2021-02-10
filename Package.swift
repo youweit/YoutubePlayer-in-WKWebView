63d8bc54bdc8dd43c1ad76670f740d6074c1b232
Crt Gregoric <crt.gregoric@gmail.com>
2021-02-10 07:22:47 +0100

    Fixed broken playback by adding required resources

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -13,7 +13,14 @@ let package = Package(
         .target(
             name: "YoutubePlayer-in-WKWebView",
             path: "WKYTPlayerView",
-            publicHeadersPath: "."
+            exclude: ["youtube-player-ios-example", "YoutubePlayer-in-WKWebView.podspec"],
+            resources: [
+                .process("WKYTPlayerView.bundle")
+            ],
+            publicHeadersPath: ".",
+            cSettings: [
+                .define("SPM_BUNDLE")
+            ]
         )
     ]
 )
